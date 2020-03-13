929891396014635d9444cd6747d1073861e1d6c3
Cahya Bagus Sanjaya <9c96b6@gmail.com>
2020-03-13 23:42:57 +0700

    Index diganti Users

---
+++ src/pages/Users.vue
@@ -0,0 +1,95 @@
+<template>
+  <q-page class="flex q-pa-xs">
+    <q-header elevated>
+      <q-toolbar>
+        <q-toolbar-title>
+          Mini Chat
+        </q-toolbar-title>
+
+        <q-btn round>
+          <q-avatar>
+            <img :src="user.photoURL">
+          </q-avatar>
+
+          <q-menu
+            transition-show="flip-right"
+            transition-hide="flip-left"
+          >
+            <q-list style="min-width: 100px">
+              <q-item clickable @click="logout">
+                <q-item-section>
+                  <q-item-label>Logout</q-item-label>
+                  <q-item-label caption>{{ user.displayName || user.email }}</q-item-label>
+                </q-item-section>
+              </q-item>
+            </q-list>
+          </q-menu>
+        </q-btn>
+      </q-toolbar>
+    </q-header>
+
+    <q-list
+      class="full-width"
+      separator>
+      <q-item
+        v-for="user in users"
+        :key="user.uid"
+        :to="{name:'chat', params: { user: user.uid } }"
+        clickable
+        v-ripple>
+        <q-item-section avatar>
+          <q-avatar color="primary" text-color="white">
+            <img :src="user.photoURL">
+          </q-avatar>
+        </q-item-section>
+
+        <q-item-section>
+          <q-item-label>{{ user.displayName }}</q-item-label>
+        </q-item-section>
+
+        <q-item-section side>
+          <q-badge
+            :color="user.online ? 'light-green-5' : 'grey-4'">
+            {{ user.online ? 'Online' : 'Offline' }}
+          </q-badge>
+        </q-item-section>
+      </q-item>
+
+    </q-list>
+  </q-page>
+</template>
+
+<script>
+export default {
+  name: 'PageUsers',
+  data () {
+    return {
+      user: this.$auth.currentUser,
+      users: []
+    }
+  },
+  firestore () {
+    return {
+      users: this.$db.collection('users')
+    }
+  },
+  methods: {
+    logout () {
+      var userId = this.user.uid
+      this.$auth.signOut().then(() => {
+        this.$db.collection('users')
+          .doc(userId)
+          .update({
+            online: false
+          })
+      }).catch((error) => {
+        this.$q.notify('error on Logout')
+        console.error(`Error on logout: ${error.message}`)
+      })
+    }
+  }
+}
+</script>
+
+<style>
+</style>
